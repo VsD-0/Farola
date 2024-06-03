1224f3b3bf9cf0e4f31f72be9d4c353a833b0457
Dmitry <vsdmitri@gmail.com>
2024-06-03 18:33:30 +0500

    Update auth

---
+++ Farola.Database/Models/RefreshToken.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Farola.Database.Models;
+
+/// <summary>
+/// Токены обновления
+/// </summary>
+public partial class RefreshToken
+{
+    /// <summary>
+    /// Идентификатор токена
+    /// </summary>
+    public int Id { get; set; }
+
+    /// <summary>
+    /// Идентификатор пользователя
+    /// </summary>
+    public int Userid { get; set; }
+
+    /// <summary>
+    /// Токен
+    /// </summary>
+    public string Token { get; set; } = null!;
+
+    /// <summary>
+    /// Дата и время создания
+    /// </summary>
+    public DateTime Createdat { get; set; }
+
+    /// <summary>
+    /// Дата и время истечения срока действия
+    /// </summary>
+    public DateTime Expiresat { get; set; }
+
+    public virtual User User { get; set; } = null!;
+}
