eba9d82d983da749f5d267ce76de91f5eb175ace
Dmitry <vsdmitri@gmail.com>
2024-06-05 22:43:58 +0500

    save

--- Farola/Program.cs
+++ Farola/Program.cs
@@ -42,6 +42,7 @@ builder.Services
     .AddHttpMessageHandler<TokenHandler>()
     .ConfigureHttpClient(refitHttp);
 
+
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddFluentUIComponents();
 
